972eefecb59caa9220a0dda2b476a364b1bb685d
MattWPhillips <mantis_rulze@hotmail.com>
2013-11-23 17:33:13 +1100

    First commit

---
+++ src/com/example/mcal/MainActivity.java
@@ -0,0 +1,42 @@
+package com.example.mcal;
+
+import android.os.Bundle;
+import android.app.ListActivity;
+import android.content.Intent;
+import android.view.Menu;
+import android.view.View;
+import android.widget.ArrayAdapter;
+import android.widget.ListView;
+
+public class MainActivity extends ListActivity {
+
+	String userOptions[] = {"NewEvent","ViewEvent","EditEvent"};
+		
+	@Override
+	protected void onCreate(Bundle savedInstanceState) {
+		super.onCreate(savedInstanceState);
+		setListAdapter(new ArrayAdapter<String>(MainActivity.this, android.R.layout.simple_list_item_1, userOptions));
+	}
+	
+	@Override
+	protected void onListItemClick(ListView l, View v, int position, long id) {
+		// TODO Auto-generated method stub
+		super.onListItemClick(l, v, position, id);
+		try {
+			Class createClass = Class.forName("com.example.mcal." + userOptions[position]);
+			Intent intent = new Intent(MainActivity.this, createClass);
+			startActivity(intent);
+		} catch (ClassNotFoundException e) {
+			e.printStackTrace();
+		}
+
+	}
+
+	@Override
+	public boolean onCreateOptionsMenu(Menu menu) {
+		// Inflate the menu; this adds items to the action bar if it is present.
+		getMenuInflater().inflate(R.menu.main, menu);
+		return true;
+	}
+
+}
